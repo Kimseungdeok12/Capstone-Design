e250beb3e466e5ddaf0b382435aa996b2b8095bd
Kim seungdeok <sdkksd6331@gmail.com>
2024-05-30 09:37:21 +0900

    second commit

--- cap/src/main.js
+++ cap/src/main.js
@@ -4,6 +4,7 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 loadFonts()
 
